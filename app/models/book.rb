6788ddb9e6ae9aada0d8ec04c192fd40491cb0c0
Jônatas Rancan de Souza <jonatasrancan@gmail.com>
2018-02-20 18:01:11 -0300

    Add more fragment cache

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,14 +1,22 @@
 class Book < ApplicationRecord
   has_many :comments
 
+  def self.top_rated(limit)
+    Book.joins(:comments)
+      .select('books.id, books.updated_at, title, avg(comments.rate) as avg_rate')
+      .group('books.id')
+      .order('avg_rate desc')
+      .limit(limit)
+  end
+
   def comments_amount
-    Rails.cache.fetch([self, 'comments_amount']) do
+    Rails.cache.fetch(['comments_amount', self]) do
       comments.size
     end
   end
 
   def rate
-    Rails.cache.fetch([self, 'rate']) do
+    Rails.cache.fetch(['rate', self]) do
       comments.sum(&:rate)/comments_amount
     end
   end
