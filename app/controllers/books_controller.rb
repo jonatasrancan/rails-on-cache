0dbfe36f1531c814cb38f0e724470622768ecfff
Jônatas Rancan de Souza <jonatasrancan@gmail.com>
2018-02-20 18:56:09 -0300

    Add conditional etag

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,5 +5,9 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+
+    if params[:etag]
+      fresh_when last_modified: @book.updated_at, etag: @book.cache_key
+    end
   end
 end
